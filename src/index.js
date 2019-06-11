0c42f4b870f27f4f390a5a4bbe26bd2975e8daf5
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-06-11 20:12:16 +0400

    continue 1st

--- src/index.js
+++ src/index.js
@@ -1,16 +1,93 @@
-require("dotenv").config();
+var fs = require("fs");
+const path = require("path");
 
-const http = require("http");
-const SERVER_PORT = process.env.PORT || 3000;
+if (process.argv.length <= 2) {
+	console.log("Usage: " + __filename + " path/to/directory");
+	process.exit(-1);
+}
 
-const { makeTimer } = require("./utils");
+const repoPath = path.normalize(`${__dirname}/..`);
+const pathToFolder = process.argv[2];
+const pathToNewFolder = process.argv[3];
 
-const server = http.createServer((request, response) => {
-	if (request.method === "GET") {
-		makeTimer(response);
+const normalizedPath = path.join(repoPath, "/", pathToFolder);
+const folderPath = path.join(repoPath, "/", pathToNewFolder);
+
+const makeDir = (pathDir, callback) => {
+	fs.mkdir(pathDir, { recursive: true }, err => {
+		if (err) {
+			if (err.code === "EEXIST") {
+				if (callback) {
+					callback();
+				}
+
+				return console.log("folder exists");
+			}
+
+			return console.log("get an error", err);
+		}
+
+		if (callback) {
+			callback();
+		}
+	});
+};
+
+const writeFile = (filePath, content) => {
+	fs.writeFile(filePath, content, err => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		console.log("file created");
+	});
+};
+
+const readFile = filePath => {
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(data.toString());
+	});
+};
+
+const readFiles = pathToFile => {
+	if (pathToFile) {
+		fs.readdir(pathToFile, function(err, items) {
+			items.forEach(item => {
+				const pathToItem = path.normalize(`${pathToFile}/${item}`);
+
+				fs.stat(pathToItem, (err, stats) => {
+					if (stats.isFile()) {
+						const itemFullName = path.basename(pathToItem);
+						const firstItemUpperCaseLetter = itemFullName[0].toUpperCase();
+						const itemContent = readFile(pathToItem);
+						const directoryPathForItem = path.join(
+							folderPath,
+							"/",
+							firstItemUpperCaseLetter
+						);
+
+						makeDir(directoryPathForItem, () => {
+							console.log("///", itemFullName);
+							writeFile(
+								path.join(directoryPathForItem, "/", itemFullName),
+								itemContent
+							);
+						});
+					} else if (stats.isDirectory()) {
+						readFiles(pathToItem);
+						// console.log("get folder");
+					}
+				});
+			});
+		});
 	}
-});
+};
 
-server.listen(SERVER_PORT, () => {
-	console.log(`Server running on port: ${SERVER_PORT}`);
-});
+makeDir(folderPath);
+
+readFiles(normalizedPath);
