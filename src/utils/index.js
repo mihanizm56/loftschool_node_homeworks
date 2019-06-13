46a20d10a935d6532534dbc70b74f9026e9825b0
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-06-14 00:22:19 +0300

    fix_2_1_hw

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,31 +1,20 @@
 const Ramda = require("ramda");
-const moment = require("moment");
 const timeToEnd = parseInt(process.env.TIME_TO_FINISH);
 const intervalIteration = parseInt(process.env.INTERVAL_TIMER);
 
 const logger = value => {
-	console.log("time", value);
+	console.log("timer", value);
 };
 
-const finishProcessWithTime = response => time => {
-	const actualDate = timeTransformer(new Date());
-
-	console.log("finished", time, new Date());
-	response.end(`stopped time ${time} ,actual time ${actualDate}`);
-};
-
-const timeTransformer = timeInMs =>
-	moment.utc(timeInMs).format("Y-MM-DD HH:mm:ss");
+const timeTransformer = timeInMs => new Date(timeInMs).toUTCString()
 
 module.exports.makeTimer = response => {
 	const now = new Date();
 	const initialTime = now.getTime();
 	const finishTime = initialTime + timeToEnd;
-	let iterableTime = initialTime;
+	const iterableTime = initialTime + intervalIteration;
 
 	const timerId = setInterval(() => {
-		iterableTime = iterableTime + intervalIteration;
-
 		Ramda.compose(
 			logger,
 			timeTransformer
@@ -33,10 +22,9 @@ module.exports.makeTimer = response => {
 	}, intervalIteration);
 
 	setTimeout(function() {
+		console.log('timer finished')
 		clearInterval(timerId);
-		Ramda.compose(
-			finishProcessWithTime(response),
-			timeTransformer
-		)(finishTime);
 	}, timeToEnd);
+
+	response.end(`actual time ${new Date().toUTCString()}`);
 };
