087d3f4bc1ff8b60558fbae9dd5c8f23ae278939
Michael_Kozhevnikov <mkozhevnikov@technoserv.com>
2019-06-22 01:04:59 +0400

    fix

--- server/src/controllers/mail-sender/index.js
+++ server/src/controllers/mail-sender/index.js
@@ -1,6 +1,27 @@
+const nodemailer = require("nodemailer");
+const config = require("../../../config.json");
+
+const sendEmail = (res, name, message, email) => {
+  console.log("check", name, message, email);
+  const transporter = nodemailer.createTransport(config.mail.stmp);
+  const mailOptions = {
+    from: `${name} ${email}`,
+    to: config.mail.stmp.auth.user,
+    subject: config.mail.subject,
+    text: message.trim().slice(0, 500) + "\n" + `отправлено с <${email}>`
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json({ message: "error when sending" });
+    }
+  });
+};
+
 const senderMessage = (req, res) => {
   const { body: { email = "", name = "", message = "" } = {} } = req;
-  if (email && name && message) {
+  if (email && name) {
+    sendEmail(res, name, message, email);
+
     res
       .status(200)
       .render("index", { msgemail: "Ваше сообщение успешно отправлено!" });
