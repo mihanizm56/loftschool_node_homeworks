9074a3da087d1ea968f632d5a8a51b82f4d7bb17
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-07-17 11:31:58 +0300

    fix

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -1,40 +1,18 @@
 let clients = {};
 
-const getfilteredUsers = (object, key) =>
-  Object.keys(object).reduce((acc, item) => {
-    if (item !== key) {
-      acc[item] = object[item];
-    }
-
-    return acc;
-  }, {});
-
 module.exports = server => {
   const io = require("socket.io")(server);
 
   io.on("connection", client => {
-    console.log("data before add", clients);
-
     const clientId = client.id;
-    const username = client.request.headers.username;
     const userData = {
       id: clientId,
-      username
+      username: client.request.headers.username
     };
 
-    clients[username] = userData;
-    const isUserExist = Boolean(clients[username]);
-    // if (!isUserExist) {
-
-    // }
-    const filteredUsers = isUserExist
-      ? getfilteredUsers(clients, username)
-      : clients;
-
-    console.log("sended data to all", filteredUsers);
-
-    client.emit("all users", filteredUsers);
-    client.broadcast.emit("new user", isUserExist ? userData : {});
+    clients[clientId] = userData;
+    client.emit("all users", clients);
+    client.broadcast.emit("new user", userData);
 
     client.on(
       "chat message",
@@ -45,9 +23,7 @@ module.exports = server => {
 
     client.on("disconnect", () => {
       client.broadcast.json.emit("delete user", clientId);
-      delete clients[username];
-      console.log("user disconnected", username);
-      console.log("data after delete", clients);
+      delete clients[clientId];
     });
   });
 };
