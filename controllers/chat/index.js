b98dfef08094c75a76fae6549aec2c459f032062
MKozhevnikov <mkozhevnikov@technoserv.com>
2019-07-17 11:30:40 +0300

    fix

--- controllers/chat/index.js
+++ controllers/chat/index.js
@@ -13,6 +13,8 @@ module.exports = server => {
   const io = require("socket.io")(server);
 
   io.on("connection", client => {
+    console.log("data before add", clients);
+
     const clientId = client.id;
     const username = client.request.headers.username;
     const userData = {
@@ -20,14 +22,19 @@ module.exports = server => {
       username
     };
 
+    clients[username] = userData;
     const isUserExist = Boolean(clients[username]);
+    // if (!isUserExist) {
+
+    // }
     const filteredUsers = isUserExist
       ? getfilteredUsers(clients, username)
       : clients;
 
+    console.log("sended data to all", filteredUsers);
+
     client.emit("all users", filteredUsers);
-    client.broadcast.emit("new user", userData);
-    clients[username] = userData;
+    client.broadcast.emit("new user", isUserExist ? userData : {});
 
     client.on(
       "chat message",
@@ -39,6 +46,8 @@ module.exports = server => {
     client.on("disconnect", () => {
       client.broadcast.json.emit("delete user", clientId);
       delete clients[username];
+      console.log("user disconnected", username);
+      console.log("data after delete", clients);
     });
   });
 };
